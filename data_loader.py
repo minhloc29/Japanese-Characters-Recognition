7a4a52df46382e32dfdf6e5227fa4f382686e753
Minh Lộc <83216771+minhloc29@users.noreply.github.com>
2024-12-07 15:29:56 +0700

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,14 +1,12 @@
-import cv2
+import tensorflow as tf
 import numpy as np
-from torch.utils.data import Dataset
-from utils import get_mask, load_image
-import torch
 import albumentations as A
+import cv2
+from utils_tensor import get_mask, load_image
 
-class JapaneseDataset(Dataset):
-    def __init__(self, image_urls, labels, batch_size, input_channels = 3, img_size = (512, 512), n_classes = 2, augment = False, shuffle = True):
-        #image_urls: list of image urls as input
-        #augment: A_Transform
+class JapaneseDataset(tf.keras.utils.Sequence):
+    def __init__(self, image_urls, labels, batch_size, input_channels=3, img_size=(512, 512), n_classes=2, augment=False, shuffle=True):
+       
         self.img_size = img_size
         self.input_channels = input_channels
         self.batch_size = batch_size
@@ -16,44 +14,59 @@ class JapaneseDataset(Dataset):
         self.n_classes = n_classes
         self.labels = labels
         self.augment = augment
-        self.ids = range(len(self.image_urls))
-        
+        self.ids = np.arange(len(self.image_urls))
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
     def __len__(self):
-        return int(len(self.ids) / self.batch_size)
-        
+        """Return the number of batches per epoch."""
+        return int(np.floor(len(self.ids) / self.batch_size))
+
     def __getitem__(self, index):
-        indexes = list(range(index * self.batch_size, (index+1)*self.batch_size))
-        temp_ids = [self.ids[i] for i in indexes]
-        X, y = self.data_generation(temp_ids)
+        """Generate one batch of data."""
+        indexes = self.ids[index * self.batch_size:(index + 1) * self.batch_size]
+        X, y = self.data_generation(indexes)
         return X, y
-    
+
+    def on_epoch_end(self):
+        """Shuffle data at the end of each epoch.""" #cuz you want each epoch to be randomly shuffle
+        if self.shuffle:
+            np.random.shuffle(self.ids)
+
     def augmentation(self):
+        """Define the augmentation pipeline using Albumentations."""
         return A.Compose([
-            A.RandomCrop(width=512, height=512, p = 0.75), #randomly crop a rectangular in the images with a specified w,h and probability
+            A.RandomCrop(width=512, height=512, p=0.75),
             A.HorizontalFlip(p=0.5),
-            A.RandomBrightnessContrast(p = 0.2),
+            A.RandomBrightnessContrast(p=0.2),
             A.ShiftScaleRotate(
-                p = 0.5, rotate_limit = 1.5,
-                scale_limit = 0.05, border_mode = 0
-            )])
-    
-    def data_generation(self, temp_ids):
-        X = np.zeros((0, *self.img_size, self.input_channels))
-        y = np.zeros((0, *self.img_size, self.n_classes))
-        for id in temp_ids:
-            img = load_image(self.image_urls[id]) #tensor, shape: CxHxW
+                p=0.5, rotate_limit=1.5,
+                scale_limit=0.05, border_mode=0
+            )
+        ])
+
+    def data_generation(self, indexes):
+        """Generate data for the batch."""
+        X = np.zeros((len(indexes), *self.img_size, self.input_channels), dtype=np.float32)
+        y = np.zeros((len(indexes), *self.img_size, self.n_classes), dtype=np.float32)
+
+        for i, idx in enumerate(indexes):
+            img = load_image(self.image_urls[idx])  # Load and preprocess image
             img = np.array(img)
-            img = np.transpose(img, (2, 1, 0)) #convert to WxHxC
-            #using [:, :, ::-1] to convert to RGB images cuz cv2 read BGR by default
-            mask = get_mask(img = self.image_urls[id], labels= self.labels[id])
+            img = np.transpose(img, (1, 0, 2))  # Convert to WxHxC
+
+            mask = get_mask(img=self.image_urls[idx], labels=self.labels[idx])
             mask = cv2.resize(mask, self.img_size)
-            
+
             if self.augment:
-                aug = self.augmentation()(image = img, mask = mask)
+                aug = self.augmentation()(image=img, mask=mask)
                 img = aug['image']
                 mask = aug['mask']
-            X = np.vstack((X, np.expand_dims(img, axis=0)))
-            y = np.vstack((y, np.expand_dims(mask, axis=0)))
-        X = torch.tensor(np.transpose(X, (0, 3, 1, 2)), dtype=torch.float32)
-        y = torch.tensor(np.transpose(y, (0, 3, 1, 2)), dtype=torch.float32)
+
+            X[i] = img
+            y[i] = mask
+
+        X = tf.convert_to_tensor(X, dtype=tf.float32)
+        y = tf.convert_to_tensor(y, dtype=tf.float32)
+
         return X, y
