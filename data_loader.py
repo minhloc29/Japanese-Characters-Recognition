cb1a57d6c0d559d15af3132ac1de100036ebebe9
Minh Lộc <83216771+minhloc29@users.noreply.github.com>
2024-12-13 08:57:13 +0700

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -2,9 +2,10 @@ import tensorflow as tf
 import numpy as np
 import cv2
 import albumentations as A
-from utils import get_mask, load_image
-
-class JapaneseDataset(tf.keras.utils.Sequence):
+import random
+from utils import get_mask, load_image, resize_padding
+#dataset for segmentation
+class JapaneseDataset:
     def __init__(self, image_urls, labels, batch_size, input_channels=3, img_size=(512, 512), n_classes=2, augment=False, shuffle=True):
        
         self.img_size = img_size
@@ -65,3 +66,59 @@ class JapaneseDataset(tf.keras.utils.Sequence):
         
         return dataset
 
+#dataset for classification
+class ClassifierDataset:
+    def __init__(self, image_urls, labels, batch_size, input_channels=3, img_size=(64, 64), augment = False, shuffle = False):
+        self.image_urls = image_urls
+        self.labels = labels
+        self.batch_size = batch_size
+        self.input_channels = input_channels
+        self.img_size = img_size
+        self.shuffle = shuffle
+        self.augment = augment
+    
+    def augmentation(self):
+        return A.Compose([
+        A.RandomBrightnessContrast(
+            contrast_limit=0.2, brightness_limit=0.2, p=0.5
+        ),  # Adjust lighting variations
+        A.GaussNoise(var_limit=(10, 50), p=0.3),  # Simulate noisy scans or photos
+        A.Blur(blur_limit=3, p=0.2),             # Simulate minor blur
+        A.Rotate(limit=5, p=0.5),                # Small rotations for robustness
+        A.ElasticTransform(alpha=1, sigma=50, alpha_affine=30, p=0.3),  # Distortions for handwriting
+    ])
+        
+    def fixup_shape(self, image, label):
+        image.set_shape([*self.img_size, 3])
+        label.set_shape([1])
+        return image, label
+    
+    def process_data(self, image_url, label):
+        image_url = image_url.numpy().decode('utf-8')
+        label = label.numpy()
+        label = np.array(label, dtype=np.int32).reshape(1)
+        image = cv2.imread(image_url)
+        image = image / 255
+        image = image.astype('float32')
+
+        mean = np.mean(image, axis=(0, 1, 2))
+        std = np.std(image, axis=(0, 1, 2))
+        image = (image - mean) / std
+        image = resize_padding(image, desire_size=64)
+        if self.augment:
+            aug = self.augmentation()(image=image)
+            image = aug['image']
+        #normalize data
+        return image, label
+    
+    def create_tf_dataset(self):
+        dataset = tf.data.Dataset.from_tensor_slices((self.image_urls, self.labels))
+        dataset = dataset.map(lambda img_url, label: tf.py_function(self.process_data, [img_url, label], [tf.float32, tf.int32]),
+                              num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        dataset = dataset.map(self.fixup_shape)
+        if self.shuffle:
+            dataset = dataset.shuffle(buffer_size=100)
+        dataset = dataset.repeat()
+        dataset = dataset.batch(self.batch_size)
+        dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+        return dataset
